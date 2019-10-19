5e20a70a2d9f8b6fac3193d3535a96293457210c
Chuangbin Chen <1552566899@qq.com>
2019-10-19 17:56:32 +0800

    add all comment

--- include/System.h
+++ include/System.h
@@ -25,7 +25,6 @@
 #include<string>
 #include<thread>
 #include<opencv2/core/core.hpp>
-#include <unistd.h>
 
 #include "Tracking.h"
 #include "FrameDrawer.h"
@@ -83,10 +82,6 @@ public:
     // This resumes local mapping thread and performs SLAM again.
     void DeactivateLocalizationMode();
 
-    // Returns true if there have been a big map change (loop closure, global BA)
-    // since last call to this function
-    bool MapChanged();
-
     // Reset the system (clear map)
     void Reset();
 
@@ -116,12 +111,6 @@ public:
     // TODO: Save/Load functions
     // SaveMap(const string &filename);
     // LoadMap(const string &filename);
-
-    // Information from most recent processed frame
-    // You can call this right after TrackMonocular (or stereo or RGBD)
-    int GetTrackingState();
-    std::vector<MapPoint*> GetTrackedMapPoints();
-    std::vector<cv::KeyPoint> GetTrackedKeyPointsUn();
 
 private:
 
@@ -169,12 +158,6 @@ private:
     std::mutex mMutexMode;
     bool mbActivateLocalizationMode;
     bool mbDeactivateLocalizationMode;
-
-    // Tracking state
-    int mTrackingState;
-    std::vector<MapPoint*> mTrackedMapPoints;
-    std::vector<cv::KeyPoint> mTrackedKeyPointsUn;
-    std::mutex mMutexState;
 };
 
 }// namespace ORB_SLAM
